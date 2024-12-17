8d47a4b21291f8dc025b0a3ad33a5d974905e571
Kim Alfred A. Molina <kimalfred22molina@gmail.com>
2024-12-17 21:09:44 +0800

    Added Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import Home from "./components/Home";
+
 const App = () => {
   return (
     <div className="relative min-h-screen">
-      <div class="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
+      <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
 
+      <div className="relative z-10">       
+        <div id="home">
+          <Home />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import foodbg from "../assets/foodbg.jpg"
+
+const Home = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen p-6">
+      <div className="text-center">
+        <h1 className="text-5xl font-bold text-white mb-4">
+          Generate your favorite dishes and recipe
+        </h1>
+        <p className="text-lg text-white mb-6">
+          Discover delectable cuisine and unforgettable moments <br />
+          in our welcoming culinary haven.
+        </p>
+        <div className="flex justify-center gap-4">
+            <button class="group relative"><div class="relative z-10 inline-flex h-12 items-center justify-center overflow-hidden rounded-md border border-neutral-200 bg-transparent px-20 font-medium text-white transition-all duration-300 group-hover:-translate-x-3 group-hover:-translate-y-3 group-hover:text-red-700 group-active:translate-x-0 group-active:translate-y-0">
+                Generate Recipe</div><div class="absolute inset-0 z-0 h-full w-full rounded-md transition-all duration-300 group-hover:-translate-x-3 group-hover:-translate-y-3 group-hover:[box-shadow:5px_5px_#a3a3a3,10px_10px_#d4d4d4,15px_15px_#e5e5e5] group-active:translate-x-0 group-active:translate-y-0 group-active:shadow-none"></div>
+            </button>   
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
